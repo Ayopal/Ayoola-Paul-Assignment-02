f54f8475095777b0c1a590210886c8f5b642d41f
Ayoola Paul <55549866+Ayopal@users.noreply.github.com>
2022-07-09 15:02:17 +0000

    challenge 1 and 3 done

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,42 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
+ 
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+// Logic
+
+var sum = 0; //initialising sum to be 0
+  
+// Method 1: Using map
+arrayOfNumbers.map(cur => sum += cur)
+  
+ // map loops over the array element and add up the current element to the recent sum total
+
+  
+/*
+Method 2: Using forEach
+arrayOfNumbers.forEach(cur => sum += cur)
+ 
+forEach loops over each element in the array and add up the current element to the recent sum total
+*/
+
+
+/* 
+Method 3: Using for of 
+  for(const cur of arrayOfNumbers){
+      sum += cur;
+    }  
+*/
+
+//this returns the final sum  
+return sum;
+
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers;
+
+
 
 /**
  * Challenge - 2
@@ -28,7 +60,18 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+//Logic
+
+let count = 0;
+//Method 1: for loop
+for(let i = 0; i < arrayOfNumbers.length; i++){
+    if(arrayOfNumbers[i]%2 === 0){
+      count++;
+    }
+  } 
+
+  
+return count;
 }
 // assignment.countEvenNumbers = countEvenNumbers;
 
@@ -47,9 +90,16 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+//Logic
+//celsiusToFahrenheit formula  °F = (°C × 9/5) + 32
+
+const fahrenheitArray = arrayOfNumbers.map(cur => Math.trunc((cur * 9/5) + 32));
+//Math.trunc remove the decimals
+ 
+return fahrenheitArray;
+
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
